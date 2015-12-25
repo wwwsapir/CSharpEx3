d17015ae8eebe07efb24e9f262797e9160f4ca6a
wwwsapir <wwwsapir@gmail.com>
2015-12-25 21:36:16 +0200

    DoorNumberFeature

---
+++ Ex03.GarageLogic/DoorsNumberFeature.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public sealed class DoorsNumberFeature : Feature
+    {
+        private eDoorsNumber m_DoorsNumber;
+
+        private enum eDoorsNumber
+        {
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5
+        }
+
+        public DoorsNumberFeature()
+        {
+            m_Description = "Doors Number";
+            m_PossibleValues = "{Two/Three/Four/Five}";
+        }
+
+        public override void SetValue(string i_ValueStr)
+        {
+            const bool v_IgnoreCaseDifferences = true;
+            bool inputValid = Enum.TryParse(i_ValueStr, v_IgnoreCaseDifferences, out m_DoorsNumber);
+            if (!inputValid)
+            {
+                throw new FormatException();
+            }
+        }
+
+        protected override object Value
+        {
+            get { return m_DoorsNumber; }
+        }
+    }
+}
