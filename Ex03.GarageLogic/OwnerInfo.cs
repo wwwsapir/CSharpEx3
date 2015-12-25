b27fbf716091e6dc6b7ca1162f1abbd1c9aa4963
wwwsapir <wwwsapir@gmail.com>
2015-12-25 23:53:39 +0200

    SapirFinishedPart25.12-23:53

---
+++ Ex03.GarageLogic/OwnerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public sealed class OwnerInfo
+    {
+        string m_Name;
+        string m_PhoneNumber;
+
+        public OwnerInfo(string i_Name, string i_PhoneNumber)
+        {
+            m_Name = i_Name;
+            m_PhoneNumber = i_PhoneNumber;
+        }
+    }
+}
---
+++ Ex03.GarageLogic/VehicleListing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public sealed class VehicleListing
+    {
+        private OwnerInfo m_OwnerInfo;
+        private VehicleInfo m_VehicleInfo;
+        private eVehicleStatus m_VehicleStatus;
+
+        public eVehicleStatus VehicleStatus
+        {
+            get { return m_VehicleStatus; }
+            set { m_VehicleStatus = value; }
+        }
+
+        public enum eVehicleStatus
+        {
+            InRepair,
+            Repaired,
+            Paid
+        }
+
+        public VehicleListing(VehicleInfo i_VehicleInfo, OwnerInfo i_OwnerInfo)
+        {
+            m_OwnerInfo = i_OwnerInfo;
+            m_VehicleInfo = i_VehicleInfo;
+        }
+    }
+}
